f6474ac2f9f1c8f2c21bbc08b6ebbe7d289b41b7
mahir-shaikh <mahirthebest95@gmail.com>
2021-07-07 22:44:04 +0530

    Added copy to clipboard functionality

--- popup.js
+++ popup.js
@@ -37,36 +37,36 @@ const setDOMInfo = info => {
     document.getElementById('bootstrapbetweenless').textContent = getBootstrapWidth(info.width).less;
 
     let bootstrapConfig = getBootstrapWidth(info.width)
-    if(!bootstrapConfig.less || !bootstrapConfig.greater){
+    if (!bootstrapConfig.less || !bootstrapConfig.greater) {
         document.getElementById('bootstrapbetween-container').style.display = 'none';
-        
-        if(!bootstrapConfig.less){
+
+        if (!bootstrapConfig.less) {
             document.getElementById('bootstraplessthan-container').style.display = 'none';
-        }else{
+        } else {
             document.getElementById('bootstrapgreaterthan-container').style.display = 'none';
         }
     }
-    
+
 };
 
-function getBootstrapWidth(screenwidth){
+function getBootstrapWidth(screenwidth) {
     // let screenSizes = [576, 768, 992, 1200]
     let response = {
         less: null,
         greater: null
     }
-    if(screenwidth < 576){
+    if (screenwidth < 576) {
         response.greater = 575.98;
-    }else if(screenwidth < 768){
+    } else if (screenwidth < 768) {
         response.less = 576;
         response.greater = 767.98;
-    }else if(screenwidth < 992){
+    } else if (screenwidth < 992) {
         response.less = 768;
         response.greater = 991.98
-    }else if(screenwidth < 1200){
+    } else if (screenwidth < 1200) {
         response.less = 992;
         response.greater = 1199.98;
-    }else{
+    } else {
         response.less = 1200;
     }
 
@@ -88,4 +88,42 @@ window.addEventListener('DOMContentLoaded', () => {
             //    from the receiving end (content script).
             setDOMInfo);
     });
+
+    const btns = document.querySelectorAll('button')
+
+    btns.forEach(btn => {
+        btn.addEventListener('click', event => {
+            copyEl( event.target.dataset.id );
+        });
+    });
 });
+
+
+
+function copyEl(Id) {
+    let range, sel;
+    let Elem = document.getElementById(Id)
+
+    // Ensure that range and selection are supported by the browsers
+    if (document.createRange && window.getSelection) {
+
+        range = document.createRange();
+        sel = window.getSelection();
+        // unselect any element in the page
+        sel.removeAllRanges();
+
+        try {
+            range.selectNodeContents(Elem);
+            sel.addRange(range);
+        } catch (e) {
+            range.selectNode(Elem);
+            sel.addRange(range);
+        }
+
+        document.execCommand('copy');
+    }
+
+    sel.removeAllRanges();
+
+};
+
